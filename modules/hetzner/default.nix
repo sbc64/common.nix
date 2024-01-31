b65778ba2e709e22db2669086f3d8a796599865a
Sebastian Bolaños <sebasheston@protonmail.com>
2024-01-31 13:18:30 +0100

    enable initrd kernelmods for hetzner module

--- modules/hetzner/default.nix
+++ modules/hetzner/default.nix
@@ -7,8 +7,15 @@
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
   ];
-  boot.loader.grub = {
-    efiSupport = lib.mkDefault true;
-    efiInstallAsRemovable = lib.mkDefault true;
+  boot = {
+    loader.grub = {
+      efiSupport = lib.mkDefault true;
+      efiInstallAsRemovable = lib.mkDefault true;
+    };
+    initrd = {
+      enable = true;
+      availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront"];
+      kernelModules = ["nvme"];
+    };
   };
 }
