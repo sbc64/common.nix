74710027f39d11750bd79c5f3c080a6f5d4fe313
Sebastian Bolaños <sebasheston@protonmail.com>
2024-03-27 19:58:56 +0100

    Remove home-manager and flock

--- flake.nix
+++ flake.nix
@@ -9,10 +9,6 @@
     # This current implementation likely leaves too many copies of nixpkgs everywhere
     stable.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "stable";
-    };
     agenix = {
       url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "stable";
