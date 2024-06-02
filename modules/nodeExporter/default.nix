5abf6b02ce07453c02e32d44ed98d23783c100ec
Sebastian Bolaños <sebasheston@protonmail.com>
2024-06-02 21:17:19 +0200

    Change node exporter listen address and stop listening on tailnet
    This change is due to the relization that it makes more sense to enable
    prometheus in agent mode, implying that metrics ports don't need to be
    exposed beyong localhost. This means that we use agent mode to push the
    local metrics to the master prometheus and the master prometheus doesn't
    need to scrape. Each node handles the metrics it needs to scrape

--- modules/nodeExporter/default.nix
+++ modules/nodeExporter/default.nix
@@ -16,30 +16,16 @@ in {
       default = 9132;
     };
   };
-  config = mkMerge [
-    (mkIf (cfg.enable) {
-      services.prometheus = {
-        exporters = {
-          node = {
-            enable = true;
-            enabledCollectors = ["systemd"];
-            port = cfg.port;
-            listenAddress = "127.0.0.1";
-          };
+  config = mkIf (cfg.enable) {
+    services.prometheus = {
+      exporters = {
+        node = {
+          enable = true;
+          enabledCollectors = ["systemd"];
+          port = cfg.port;
+          listenAddress = "127.0.0.1";
         };
       };
-    })
-    (mkIf (config.tailnet.enable) {
-      services.prometheus.exporters.node = {
-        listenAddress = lib.mkForce "0.0.0.0";
-      };
-      networking.firewall = {
-        filterForward = mkDefault true;
-        extraInputRules = ''
-          iifname tailscale0 tcp dport ${builtins.toString cfg.port} accept
-          tcp dport ${builtins.toString cfg.port} drop
-        '';
-      };
-    })
-  ];
+    };
+  };
 }
