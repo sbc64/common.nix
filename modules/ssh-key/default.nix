069297457e86a823321c987ab22fffb750c738ca
Sebastian Bolaños <sebasheston@protonmail.com>
2024-06-18 22:09:43 +0200

    flock

--- modules/ssh-key/default.nix
+++ modules/ssh-key/default.nix
@@ -1,5 +1,5 @@
 let
-  mbpPersonal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHUAbxX6TG1lsVbPPG4qk5GTFP2H7rDjRpGkFWqXKB2I sbc64@pm.me";
+  mbpPersonal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHUAbxX6TG1lsVbPPG4qk5GTFP2H7rDjRpGkFWqXKB2I mbp";
   hetzner = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOXBdDTnFM5P63mcSZE1i+dYEqzVmYvv3O30hwAhCals hetzner";
 in [
   mbpPersonal
