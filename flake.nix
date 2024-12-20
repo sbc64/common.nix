2b58a4851a7e4182deab19dbb38435aa34472d6e
Sebastian Bolanos <git@sebas.tech>
2024-12-20 11:06:48 +0100

    add stable nixos and pi4 base sd

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
     #nixpkgs-lib.url = "github:NixOS/nixpkgs/nixos-unstable";
     # This current implementation likely leaves too many copies of nixpkgs everywhere
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    stable.url = "github:NixOS/nixpkgs/nixos-23.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "unstable";
@@ -25,9 +26,11 @@
   outputs = {
     self,
     unstable,
+    stable,
     ...
   } @ inputs: let
     libx = import ./lib unstable.lib self.nixosModules;
+    stateVersion = "23.11";
   in {
     # TODO extend the standard library to include your personal functions, that
     # way you only use the extended library in all your other repos
@@ -47,20 +50,57 @@
       // {
         agenix = inputs.agenix.nixosModules.default;
         disko = inputs.disko.nixosModules.disko;
+        pi4Base = {...}: {
+          imports = [
+            "${stable}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+            "${stable}/nixos/modules/installer/cd-dvd/channel.nix"
+            self.nixosModules.common
+          ];
+          networking = {
+            wireless = {
+              enable = true;
+              networks."Atila".psk = "atilaeluno9395";
+            };
+          };
+          system.activationScripts.installConfiguration = ''
+             if [ ! -f /etc/nixos/configuration.nix ]; then
+                 mkdir -p /etc/nixos
+            fi
+          '';
+          #cp -r ${./nixos}/* /etc/nixos
+          fileSystems."/" = {
+            device = "/dev/disk/by-label/NIXOS_SD";
+            fsType = "ext4";
+          };
+        };
       };
 
-    nixosConfigurations.vm = (libx self.outPath).mkHost {
-      hostname = "vm";
-      system = "aarch64-linux";
-      stateVersion = "24.05";
-      extraModules = [
-        self.nixosModules.vm
-        {
-          virtualisation.vmVariant.virtualisation.host.pkgs = unstable.legacyPackages.aarch64-darwin;
-        }
-      ];
+    nixosConfigurations = {
+      pi4Base = (libx self.outPath).mkHost {
+        inherit stateVersion;
+        system = "aarch64-linux";
+        hostname = "nixos";
+        extraModules = [
+          self.nixosModules.pi4Base
+        ];
+      };
+      vm = (libx self.outPath).mkHost {
+        hostname = "vm";
+        system = "aarch64-linux";
+        inherit stateVersion;
+        extraModules = [
+          self.nixosModules.vm
+          {
+            virtualisation.vmVariant.virtualisation.host.pkgs = unstable.legacyPackages.aarch64-darwin;
+          }
+        ];
+      };
     };
-    packages.aarch64-darwin.vm = self.nixosConfigurations.vm.config.system.build.vm;
+
+    packages = {
+      aarch64-darwin.vm = self.nixosConfigurations.vm.config.system.build.vm;
+      aarch64-linux.pi4SDBase = self.nixosConfigurations.pi4Base.config.system.build.sdImage;
+    };
     apps.aarch64-darwin.default = {
       type = "app";
       program = "${
