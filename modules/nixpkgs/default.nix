1d65e6e7a29c31bca61a0e48ffd34d2e7011f32a
Sebastian Bolanos <git@sebas.tech>
2024-12-20 11:06:48 +0100

    Add colema nixpkgs

--- modules/nixpkgs/default.nix
+++ modules/nixpkgs/default.nix
@@ -14,12 +14,14 @@
         "https://cache.nixos.org/"
         "https://nix-community.cachix.org"
         "https://nixos-dash-docset.cachix.org"
+        "https://colmena.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
         "nixos-dash-docset.cachix.org-1:CTP+Rqm1pnWU0lFaCdnN3wZmGkVnB/xoTV/SQ4T1gAU="
+        "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
       ];
       experimental-features = ["nix-command" "flakes"];
     };
