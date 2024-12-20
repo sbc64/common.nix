5086e7677defb902ade110c6a0e8528ecf7b2a13
Sebastian Bolanos <git@sebas.tech>
2024-12-20 11:06:48 +0100

    fixes to remote-builder mod

--- modules/remote-builds/default.nix
+++ modules/remote-builds/default.nix
@@ -6,19 +6,26 @@
 }: let
   builderKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICllOC9xAgR6ROJSFotRNrfErKGayL+lVd0fjC3g3VtG";
 in {
+  users.groups.builder = {
+    members = [ "builder" "sebas" "root" ];
+  };
   users.users."builder" = {
-    isSystemUser = true;
-    group = "@builder";
-    openssh.authorizedKeys = [
+    isNormalUser = true;
+    group = "builder";
+    openssh.authorizedKeys.keys = [
       builderKey
     ];
   };
   nix = {
-    settings.trusted-users = ["@builder"];
+    settings = {
+      trusted-users = ["@builder"];
+
+      system-features = ["big-parallel" "kvm"];
+    };
     sshServe = {
       enable = true;
       protocol = "ssh-ng";
-      nix.sshServe.keys = [
+      keys = [
         builderKey
       ];
     };
