4c0020d6e98a4add6184307151acc2007eecc760
Sebastian Bolanos <git@sebas.tech>
2024-12-20 11:06:48 +0100

    improve secret key progragation

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,11 +1,13 @@
 let
   #builder = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICllOC9xAgR6ROJSFotRNrfErKGayL+lVd0fjC3g3VtG builder@localhost";
   mbpMe = import ../modules/ssh-key;
-  me = [
-    # This is the mini key, disabling for now because I need to rotate it out
-    #"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAB/hlAvEO3FKG2I4aCmV/3v0Ebenh5Ye3QTo9LXzA4O sebas@mini"
-    #builder
-  ] ++ mbpMe;
+  me =
+    [
+      # This is the mini key, disabling for now because I need to rotate it out
+      #"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAB/hlAvEO3FKG2I4aCmV/3v0Ebenh5Ye3QTo9LXzA4O sebas@mini"
+      #builder
+    ]
+    ++ mbpMe;
 in {
   "me".publicKeys = me;
 }
