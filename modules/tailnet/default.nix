0b47925171ff12f9a06021fa55bdde0fba897e9a
Sebastian Bolanos <git@sebas.tech>
2024-12-20 11:06:48 +0100

    remove exit node capabilities from tailscale and rm oneshot script

--- modules/tailnet/default.nix
+++ modules/tailnet/default.nix
@@ -30,43 +30,19 @@ in {
     };
   };
   config = mkIf (cfg.enable) {
-    services.tailscale.enable = true;
+    services.tailscale = {
+      enable = true;
+      extraUpFlags = [
+        "${lib.optionalString cfg.allowSSH "--ssh"}"
+      ];
+      authKeyFile = config.age.secrets.tsAuthKey.path;
+    };
     age.secrets."tsAuthKey" = {
       file = "${callingFlakePath}/secrets/tailscale-auth.age";
     };
     networking = {
       firewall.trustedInterfaces = ["tailscale0"];
       firewall.allowedUDPPorts = [config.services.tailscale.port];
-    };
-    systemd.services.tailscale-autoconnect = {
-      description = "Automatic connection to Tailscale";
-
-      # make sure tailscale is running before trying to connect to tailscale
-      after = ["network-pre.target" "tailscale.service"];
-      wants = ["network-pre.target" "tailscale.service"];
-      wantedBy = ["multi-user.target"];
-
-      # set this service as a oneshot job
-      serviceConfig.Type = "oneshot";
-
-      # have the job run this shell script
-      script = with pkgs; ''
-        echo "Checking if already authenticated to Tailscale ..."
-        status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-        if [ $status = "Running" ]; then  # do nothing
-             echo "Already authenticated to Tailscale, exiting."
-          exit 0
-        fi
-
-        echo "Authenticating with Tailscale ..."
-        ${tailscale}/bin/tailscale up \
-          --auth-key file:${config.age.secrets.tsAuthKey.path} \
-          ${lib.optionalString cfg.allowSSH "--ssh"} \
-          ${
-          lib.optionalString (cfg.exitNode != "") "--exit-node=${cfg.exitNode} \
-            --exit-node-allow-lan-access=${builtins.toString cfg.allowLanAccess}"
-        }
-      '';
     };
   };
 }
