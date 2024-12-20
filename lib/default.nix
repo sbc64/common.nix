6452c9b113f6d9101276a49a82f2e1f78355f557
Sebastian Bolanos <git@sebas.tech>
2024-12-20 11:06:48 +0100

    add mkColmena

--- lib/default.nix
+++ lib/default.nix
@@ -1,20 +1,4 @@
 lib: libModules: callingFlakePath: {
-  /*
-  mkHome = {
-    hostname,
-    username,
-    desktop ? null,
-    platform ? "x86_64-linux",
-  }:
-    inputs.home-manager.lib.homeManagerConfiguration {
-      pkgs = inputs.nixpkgs.legacyPackages.${platform};
-      extraSpecialArgs = {
-        inherit inputs outputs desktop hostname platform username stateVersion;
-      };
-      modules = [../home-manager];
-    };
-  */
-
   mkHost = {
     hostname,
     stateVersion,
@@ -66,91 +50,29 @@ lib: libModules: callingFlakePath: {
         ++ extraModules;
     };
 
-  mkDeploy = {
-    ipOrHostname,
-    self,
-    nixosConfiguration ? "",
-    sshUser ? "root",
-    sshOptsArg ? [],
-    fastConnection ? false,
-    remoteBuild ? true,
-    magicRollback ? true,
+  mkColmena = {
+    description ? "",
+    configurations,
+    nixpkgs,
+    deployments,
   }: let
-    config =
-      if nixosConfiguration == ""
-      then ipOrHostname
-      else nixosConfiguration;
-    cfg = self.nixosConfigurations."${config}";
-    system = cfg.config.nixpkgs.system;
-    sshOpts =
-      [
-        /*
-        (if (sshOptsArg == []) then
-        "-p" (builtins.toString (builtins.elemAt cfg.config.services.openssh.ports 0))
-        else sshOptsArg)
-        */
-      ]
-      ++ sshOptsArg;
-  in {
-    inherit fastConnection remoteBuild sshUser magicRollback sshOpts;
-    hostname = ipOrHostname;
-    profiles.system = {
-      user = sshUser;
-      path = self.inputs.deploy-rs.lib."${system}".activate.nixos cfg;
-    };
-  };
-  ## Create deployment configuration for use with deploy-rs.
-  ##
-  ## ```nix
-  ## mkDeploy {
-  ##   inherit self;
-  ##   overrides = {
-  ##     my-host.system.sudo = "doas -u";
-  ##   };
-  ## }
-  ## ```
-  ##
-  #@ { self: Flake, overrides: Attrs ? {} } -> Attrs
-  mkDeploy2 = {
-    self,
-    overrides ? {},
-  }: let
-    hosts = self.nixosConfigurations or {};
-    names = builtins.attrNames hosts;
-    nodes =
-      lib.foldl
-      (result: name: let
-        host = hosts.${name};
-        user = host.config.plusultra.user.name or null;
-        inherit (host.pkgs) system;
-      in
-        result
-        // {
-          ${name} =
-            (overrides.${name} or {})
-            // {
-              hostname = overrides.${name}.hostname or "${name}";
-              profiles =
-                (overrides.${name}.profiles or {})
-                // {
-                  system =
-                    (overrides.${name}.profiles.system or {})
-                    // {
-                      path = self.inputs.deploy-rs.lib.${system}.activate.nixos host;
-                    }
-                    // lib.optionalAttrs (user != null) {
-                      user = "root";
-                      sshUser = user;
-                    }
-                    // lib.optionalAttrs
-                    (host.config.plusultra.security.doas.enable or false)
-                    {
-                      sudo = "doas -u";
-                    };
-                };
-            };
-        })
-      {}
-      names;
-  in {inherit nodes;};
+    inherit (builtins) mapAttrs elemAt filter;
+  in
+    {
+      meta = {
+        inherit description nixpkgs;
+        nodeNixpkgs = mapAttrs (name: value: value.pkgs) configurations;
+        nodeSpecialArgs = mapAttrs (name: value: value._module.specialArgs) configurations;
+      };
+    }
+    // mapAttrs (name: value: {
+      imports =
+        value._module.args.modules
+        ++ [
+          {
+            deployment = elemAt (filter (v: v.targetHost == name) deployments) 0;
+          }
+        ];
+    })
+    configurations;
 }
