53d31c0cd82e7480916b78bfc441019f324398cc
Sebastian Bolanos <git@sebas.tech>
2025-11-10 08:06:42 +0100

    Update tailscale

--- nixosModules/tailnet/default.nix
+++ nixosModules/tailnet/default.nix
@@ -22,7 +22,7 @@ in
     allowLanAccess = mkOption {
       description = "Allow lan access";
       type = types.bool;
-      default = true;
+      default = false;
     };
     exitNode = mkOption {
       description = "Exit node ip address";
@@ -36,7 +36,31 @@ in
     };
   };
   config = mkIf (cfg.enable) {
+    systemd.services.tailscaled.serviceConfig = {
+      Environment = ''"FLAGS=--tun tailscale0 --verbose=1 "'';
+    };
     services.tailscale = {
+      # TODO, move this to a package overlay of this repo or just grab an ovelray form nixos-stagin branch
+      package = (pkgs.tailscale.overrideAttrs (_: rec {
+        version = "1.90.4";
+        src = pkgs.fetchFromGitHub {
+          owner = "tailscale";
+          repo = "tailscale";
+          tag = "v${version}";
+          hash = "sha256-0d6NwGH79TFqOpbu8EUw/lhk+1gF7TupVxFW18pqRmc=";
+        };
+        vendorHash = "sha256-rV3C2Vi48FCifGt58OdEO4+Av0HRIs8sUJVvp/gEBLw=";
+      })).override {
+        buildGoModule = pkgs.buildGoModule.override {
+          go = pkgs.buildPackages.go_1_25.overrideAttrs (_: rec {
+            version = "1.25.3";
+            src = pkgs.fetchurl {
+              url = "https://go.dev/dl/go${version}.src.tar.gz";
+              hash = "sha256-qBpLpZPQAV4QxR4mfeP/B8eskU38oDfZUX0ClRcJd5U=";
+            };
+          });
+        };
+      };
       enable = true;
       extraUpFlags =
         [ ]
@@ -55,7 +79,6 @@ in
           then [ "--exit-node-allow-lan-access=true" ]
           else [ ]
         );
-
       authKeyFile = config.age.secrets.tsAuthKey.path;
       useRoutingFeatures = "client";
     };
