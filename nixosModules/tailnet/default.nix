806748d850ba0278e9197068f8cfd1086ae2ee9f
Sebastian Bolanos <git@sebas.tech>
2025-11-14 13:53:34 +0100

    Remove tailscale update

--- nixosModules/tailnet/default.nix
+++ nixosModules/tailnet/default.nix
@@ -40,28 +40,7 @@ in
       Environment = ''"FLAGS=--tun tailscale0 --verbose=1 "'';
     };
     services.tailscale = {
-      # TODO, move this to a package overlay of this repo or just grab an ovelray form nixos-stagin branch
-      package = (pkgs.tailscale.overrideAttrs (_: rec {
-        version = "1.90.4";
-        src = pkgs.fetchFromGitHub {
-          owner = "tailscale";
-          repo = "tailscale";
-          tag = "v${version}";
-          hash = "sha256-0d6NwGH79TFqOpbu8EUw/lhk+1gF7TupVxFW18pqRmc=";
-        };
-        vendorHash = "sha256-rV3C2Vi48FCifGt58OdEO4+Av0HRIs8sUJVvp/gEBLw=";
-      })).override {
-        buildGoModule = pkgs.buildGoModule.override {
-          go = pkgs.buildPackages.go_1_25.overrideAttrs (_: rec {
-            version = "1.25.3";
-            src = pkgs.fetchurl {
-              url = "https://go.dev/dl/go${version}.src.tar.gz";
-              hash = "sha256-qBpLpZPQAV4QxR4mfeP/B8eskU38oDfZUX0ClRcJd5U=";
-            };
-          });
-        };
-      };
-      enable = true;
+      enable = lib.mkDefault true;
       extraUpFlags =
         [ ]
         ++ (
